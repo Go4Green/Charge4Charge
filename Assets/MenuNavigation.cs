88e5e449ba0fe0c860f2cdbbdc22fbcce5af7cb9
Konstantinos <kostas24061992@yahoo.com>
2019-12-15 15:45:12 +0200

    Initial Commit

---
+++ Assets/MenuNavigation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour {
+
+	public Color activatedClr,deactivatedClr;
+	public Button map,shops;
+	public GameObject mapPanel,shopPanel;
+	// Use this for initialization
+	void Start () {
+		OpenShop();
+	}
+
+	public void OpenMap(){
+		map.image.color=activatedClr;
+		shops.image.color=deactivatedClr;
+		mapPanel.gameObject.SetActive(true);
+		shopPanel.gameObject.SetActive(false);
+	}
+
+	public void OpenShop(){
+		map.image.color=deactivatedClr;
+		shops.image.color=activatedClr;
+		mapPanel.gameObject.SetActive(false);
+		shopPanel.gameObject.SetActive(true);
+	}
+	
+}
---
+++ Assets/ToggleOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOptions : MonoBehaviour
+{
+
+    public GameObject filters, search, favorite;
+
+    private bool areActive = true;
+    // Use this for initialization
+    void Start()
+    {
+       ToggleOptionsBtn();
+    }
+
+    public void ToggleOptionsBtn()
+    {
+		 areActive = !areActive;
+        filters.SetActive(areActive);
+        search.SetActive(areActive);
+        favorite.SetActive(areActive);
+    }
+
+
+}
